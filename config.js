f367321f5ba93b28167a28a141f36f3de0b542ae
Palak Patel <palakkp2006@gmail.com>
2026-06-26 15:51:04 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    API_KEY: "1ffae99150msh6c2dd9465acf94ap18f99fjsn52bece0df7e1",
+    API_KEY: "OUR_RAPIDAPI_KEY",
     API_HOST: "spotify-downloader9.p.rapidapi.com",
     API_URL: "https://spotify-downloader9.p.rapidapi.com/downloadSong"
 };
